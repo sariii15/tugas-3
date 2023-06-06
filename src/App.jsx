06212c7b6aebaa2471666a2273c4b27a62e31a7f
bungahts15@gmail.com <bungahts15@gmail.com>
2023-06-06 16:27:55 +0700

    sudah selesai

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import Product from "./components/Product";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Product />
+    </>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import "./header.css";
+const Header = () => {
+  return (
+    <header>
+      <h1>Bunga sari Hutasuhut</h1>
+    </header>
+  );
+};
+
+export default Header;
